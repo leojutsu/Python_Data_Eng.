7ef3bf39a96118dba30aeb8f20d13bc170f3a4fe
Leonardo da Silva Neves <leojutsu@gmail.com>
2024-09-10 10:57:02 -0300

    feat: exer_atribuições com python

---
+++ operadores_atribuicao.py
@@ -0,0 +1,40 @@
+# Atribuição Simples
+Saldo = 300
+
+# Atribuição com adição
+
+Saldo += 100
+print(Saldo)
+# Atribuição com subtração
+
+Saldo -= 100
+print(Saldo)
+
+# Atribuição com multiplicação
+
+Saldo *= 2
+print(Saldo)
+
+# Atribuição com divisão Normal
+
+Saldo /= 5 
+print(Saldo)
+
+# Atribuição com divisão inteira
+
+Saldo //= 5
+print(Saldo)
+
+# Atribuição com módulo
+
+Saldo %= 480
+print(Saldo)
+
+# Atribuição com exponenciação
+
+Saldo **= 2
+print(Saldo)
+
+
+
+
