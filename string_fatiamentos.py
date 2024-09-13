d642b468d0c9b62241f23547c11d1dd6102f2509
Leonardo da Silva Neves <leojutsu@gmail.com>
2024-09-13 10:08:14 -0300

    docs: inclusão de info

--- string_fatiamentos.py
+++ string_fatiamentos.py
@@ -39,7 +39,6 @@ utilizando f strings.
 A primeira forma não é atualmente recomendads e seu uso Ṕython é raro, por
 esse motivo iremos focar nas 2 últimas.
 
-"""
 #primeira forma
 
 nome = "leonardo"
@@ -58,3 +57,43 @@ print("Nome: {nome} Idade {idade}" .format(nome=nome, idade=idade))
 print("Nome: {nome} Idade {idade}" .format(**dados))
 
 print(f"nome: {nome} Idade {idade} saldo {saldo:.2f}")
+
+Fatiamento
+
+Fatiamento é uma técnica utilizada para retornar substrings (partes da string original),
+informando início (start), fim(stop) e passo (step): [start:stop[, step]].
+
+nome = "Leonardo da Silva Neves"
+
+print(nome[0])
+
+print(nome[:9])
+
+print(nome[10:])
+
+print(nome[10:16])
+
+print(nome[10:16:2])
+
+print(nome[:])
+
+print(nome[::-1])
+
+Strings multiplas linhas
+
+Strings de múltiplas linhas são definidas informando 3 aspas simples ou duplas
+durante a atribuição. Elas podem oculpar várias linhas do código, e todos os
+espaços, em branco são incluídos na string final.
+
+
+
+"""
+
+nome = "Leonardo"
+
+mensagem = f'''
+  Olá meu nome é {nome},
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.
+    '''
+print(mensagem)
